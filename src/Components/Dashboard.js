b825d4cc35ec1e83edb130b7714d6bb14d00eeb8
Faithful1 <faithfulanere@gmail.com>
2017-08-02 21:08:23 +0800

    First Commit

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Row>
+            <Col xs={9} md={9}>
+              <h1>Dashboard</h1>
+              <h1>welcome to the dashboard</h1>
+            </Col>
+            <Col xs={3} md={3}>
+              <img src={this.props.profile.picture} role="presentation" />
+              <h3>
+                {this.props.profile.nickname}
+              </h3>
+              <strong>
+                {this.props.profile.email}
+              </strong>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
